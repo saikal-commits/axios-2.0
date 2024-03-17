9b09d996b62f5c49d8b58dc2adcf35fb498e849d
unknown <sajkal949@gmail.com>
2024-03-17 15:42:24 +0300

    tryuiouy

--- dogs.js
+++ dogs.js
@@ -1,17 +1,43 @@
-const buttons = document.querySelector(".buttons");
-const selBbtn = document.querySelector(".select-btn");
+const breeds = document.querySelector(".buttons");
+const dog = document.querySelector(".select-btn");
+const img = document.querySelectorAll(".breeds-img");
 
-fetch(`https://dog.ceo/api/breeds/list/all`)
-  .then((res) => res.json())
-  .then((data) => {
-    let dogs = Object.keys(data.message);
-    dogs.forEach((el) => {
-      buttons.innerHTML += `
+function getAll() {
+  axios(`https://dog.ceo/api/breeds/list/all`)
+    .then((res) => {
+      console.log(Object.keys(res.data.message));
+      Object.keys(res.data.message).forEach((el) => {
+        breeds.innerHTML += `
         <button class="btn-button">${el}</button> `;
-      selBbtn.innerHTML += `
-    <option value="dogs">${el}</option>
-    `;
+        dog.innerHTML += `
+       <option value="${el}">${el}</option>`;
+      });
+    })
+    .then(() => btn());
+}
+getAll();
+
+function getImg(name) {
+  axios(`https://dog.ceo/api/breed/${name}/images/random`).then((res) => {
+    console.log(res.data, "akita");
+    img.forEach((element) => {
+      element.innerHTML = `
+          <img src="${res.data.message}" alt="">
+        `;
     });
   });
+}
 
-  
+function btn() {
+  const buttons = document.querySelectorAll(".btn-button");
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", () => {
+      getImg(btn.innerHTML);
+    });
+  });
+}
+
+dog.addEventListener("change", (e) => {
+  console.log(e.target.value);
+  getImg(e.target.value);
+});
